f3bf103d86920186aa0da36d95466568b5ee93fb
Sebastian Francis Taylor <me@sebastiantalor.com>
2026-09-23 20:52:17 +0200

    home manager wired into laptop

--- flake.nix
+++ flake.nix
@@ -1,12 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./hosts/laptop ];
+      modules = [
+        ./hosts/laptop
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.taylor = import ./home;
+        }
+      ];
     };
   };
 }
